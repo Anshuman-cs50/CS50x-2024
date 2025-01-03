feafa42273fc45243c98a1c49680774f7e88dcbe
Anshuman Singh <155361326+Anshuman-cs50@users.noreply.github.com>
2025-01-03 16:46:53 +0530

    Add files via upload

---
+++ filter-more/helpers.c
@@ -0,0 +1,180 @@
+#include "helpers.h"
+#include <math.h>
+#include <stdio.h>
+
+void swap(BYTE *x, BYTE *y);
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float red = image[i][j].rgbtRed;
+            float green = image[i][j].rgbtGreen;
+            float blue = image[i][j].rgbtBlue;
+
+            int average = round((red + green + blue) / 3);
+
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            swap(&image[i][j].rgbtRed, &image[i][width - (j + 1)].rgbtRed);
+            swap(&image[i][j].rgbtGreen, &image[i][width - (j + 1)].rgbtGreen);
+            swap(&image[i][j].rgbtBlue, &image[i][width - (j + 1)].rgbtBlue);
+        }
+    }
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE cpy_image[height][width];
+
+    for (int a = 0; a < height; a++)
+    {
+        for (int b = 0; b < width; b++)
+        {
+            cpy_image[a][b] = image[a][b];
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int red, green, blue; // Sum of rgb values of surrounding pixels
+            red = green = blue = 0;
+
+            int pixel_count = 0; // Count of total pixels
+
+            // setting range of surrounding pixels
+            int start_height = ((i - 1) < 0) ? i : (i - 1);
+            int end_height = ((i + 1) == height) ? i : (i + 1);
+
+            int start_width = ((j - 1) < 0) ? j : (j - 1);
+            int end_width = ((j + 1) == width) ? j : (j + 1);
+
+            for (int k = start_height; k <= end_height; k++)
+            {
+                for (int l = start_width; l <= end_width; l++)
+                {
+                    red += cpy_image[k][l].rgbtRed;
+                    green += cpy_image[k][l].rgbtGreen;
+                    blue += cpy_image[k][l].rgbtBlue;
+
+                    pixel_count++;
+                }
+            }
+
+            RGBTRIPLE average;
+
+            average.rgbtRed = round((float) red / pixel_count);
+            average.rgbtGreen = round((float) green / pixel_count);
+            average.rgbtBlue = round((float) blue / pixel_count);
+
+            image[i][j] = average;
+        }
+    }
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Create temp array
+    RGBTRIPLE temp[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            temp[i][j] = image[i][j];
+        }
+    }
+    // Initialise Sobel arrays
+    int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+    // Loop through rows
+    for (int i = 0; i < height; i++)
+    {
+        // Loop through columns
+        for (int j = 0; j < width; j++)
+        {
+            // Initialise ints
+            float Gx_red;
+            float Gx_blue;
+            float Gx_green;
+            float Gy_red;
+            float Gy_blue;
+            float Gy_green;
+            Gx_red = Gx_blue = Gx_green = Gy_red = Gy_blue = Gy_green = 0;
+            // For each pixel, loop vertical and horizontal
+            for (int k = -1; k < 2; k++)
+            {
+                for (int l = -1; l < 2; l++)
+                {
+                    // Check if pixel is outside rows
+                    if (i + k < 0 || i + k >= height)
+                    {
+                        continue;
+                    }
+                    // Check if pixel is outside columns
+                    if (j + l < 0 || j + l >= width)
+                    {
+                        continue;
+                    }
+                    // Otherwise add to sums
+                    Gx_red += temp[i + k][j + l].rgbtRed * Gx[k + 1][l + 1];
+                    Gx_green += temp[i + k][j + l].rgbtGreen * Gx[k + 1][l + 1];
+                    Gx_blue += temp[i + k][j + l].rgbtBlue * Gx[k + 1][l + 1];
+                    Gy_red += temp[i + k][j + l].rgbtRed * Gy[k + 1][l + 1];
+                    Gy_green += temp[i + k][j + l].rgbtGreen * Gy[k + 1][l + 1];
+                    Gy_blue += temp[i + k][j + l].rgbtBlue * Gy[k + 1][l + 1];
+                }
+            }
+            // Calculate Sobel operator
+            int red = round(sqrt(Gx_red * Gx_red + Gy_red * Gy_red));
+            int green = round(sqrt(Gx_green * Gx_green + Gy_green * Gy_green));
+            int blue = round(sqrt(Gx_blue * Gx_blue + Gy_blue * Gy_blue));
+            // Cap at 255
+            if (red > 255)
+            {
+                red = 255;
+            }
+            if (green > 255)
+            {
+                green = 255;
+            }
+            if (blue > 255)
+            {
+                blue = 255;
+            }
+            // Assign new values to pixels
+            image[i][j].rgbtRed = red;
+            image[i][j].rgbtGreen = green;
+            image[i][j].rgbtBlue = blue;
+        }
+    }
+    return;
+}
+
+void swap(BYTE *x, BYTE *y)
+{
+    BYTE temp = *x;
+    *x = *y;
+    *y = temp;
+
+    return;
+}
